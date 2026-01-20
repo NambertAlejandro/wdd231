3e5dca940dbc09b9e4bcb3ee13f95dfe7f990a8d
NambertAlejandro <nambertprofessional.org@gmail.com>
2026-01-19 21:12:38 -0300

    add folders and quit folders

---
+++ scripts/prophets.js
@@ -0,0 +1,51 @@
+const url = 'https://byui-cse.github.io/cse-ww-program/data/latter-day-prophets.json';
+const cards = document.querySelector('#cards');
+
+const displayProphets = (prophets) => {
+  prophets.forEach((prophet) => {
+
+    const card = document.createElement('section');
+    const fullName = document.createElement('h2');
+    const portrait = document.createElement('img');
+
+ 
+    fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+
+
+    portrait.setAttribute('src', prophet.imageurl);
+    portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
+    portrait.setAttribute('loading', 'lazy');
+    portrait.setAttribute('width', '340');
+    portrait.setAttribute('height', '440');
+
+
+    card.appendChild(fullName);
+    card.appendChild(portrait);
+
+    cards.appendChild(card);
+    getProphetData();
+    const birthDate = document.createElement('p');
+    birthDate.textContent = `Date of Birth: ${prophet.birthdate}`;
+
+
+    const birthPlace = document.createElement('p');
+    birthPlace.textContent = `Place of Birth: ${prophet.birthplace}`;
+
+
+    card.appendChild(fullName);
+    card.appendChild(birthDate);
+    card.appendChild(birthPlace);
+    card.appendChild(portrait);
+  });
+};
+
+const getProphetData = async () => {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.table(data.prophets); 
+  displayProphets(data.prophets);
+};
+
+
+getProphetData();
+
